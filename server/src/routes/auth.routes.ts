763fb05f4473fac928104423647e7e993a64c260
elena-front <shumkova.helen@yandex.ru>
2026-04-03 09:52:12 +0400

    fixes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -7,9 +7,10 @@ import {
   signOut,
 } from "../controllers/Auth.controller";
 import { verifyRefreshToken } from "../middleware/verifyRefreshToken";
+import { verifyAccessToken } from "../middleware/verifyAccessToken";
 
 export default Router()
   .post("/signup", signUp)
   .post("/signin", signIn)
   .get("/refresh", verifyRefreshToken, refresh)
-  .get("/signout", signOut);
+  .get("/signout", verifyAccessToken, signOut);
